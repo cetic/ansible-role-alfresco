474f49d62a9b377d5e203a82752e0ed92248f29c
Alexandre Nuttinck <alexandre.nuttinck@cetic.be>
2018-03-13 11:15:47 +0100

    Update defaults variables

--- files/start_oo.sh
+++ files/start_oo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 SOFFICE_ROOT=/usr/bin
-"${SOFFICE_ROOT}/soffice" "--accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" --nologo --headless &
+"${SOFFICE_ROOT}/soffice" "--accept=socket,host=localhost,port={{ tomcat_port }};urp;StarOffice.ServiceManager" --nologo --headless &
